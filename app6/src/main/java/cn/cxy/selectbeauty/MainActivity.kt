94409a0fa7fdb43de9a98a96f1067bbef9eb3c5f
jerry <309032663@qq.com>
2020-10-29 15:57:47 +0800

    update

--- app6/src/main/java/cn/cxy/selectbeauty/MainActivity.kt
+++ app6/src/main/java/cn/cxy/selectbeauty/MainActivity.kt
@@ -1,20 +1,27 @@
 package cn.cxy.selectbeauty
 
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.util.TypedValue
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.animation.Animation
 import android.view.animation.ScaleAnimation
 import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.children
+import androidx.core.view.setPadding
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.util.*
 import kotlin.concurrent.fixedRateTimer
 
@@ -24,6 +31,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initListeners()
+        queryData()
+    }
+
+    private fun initListeners() {
         button.setOnClickListener {
             if (button.text != "停") {
                 countDownTv.visibility = VISIBLE
@@ -90,5 +102,39 @@ class MainActivity : AppCompatActivity() {
 
     private fun clearImageBackground(view: View) {
         view.setBackgroundColor(Color.parseColor("#FFFFFF"))
+    }
+
+    private fun queryData() {
+        val networkService = getNetworkService()
+        GlobalScope.launch(Dispatchers.Main) {
+            val result = withContext(Dispatchers.IO) { networkService.query() }
+            result.forEach { addNewViewByUrl(it.url) }
+        }
+    }
+
+    private fun addNewViewByUrl(url: String) {
+        val imageView = ImageView(this)
+        imageView.setPadding(dp2px(5))
+        Glide.with(this).load(url).into(imageView)
+        val layoutParams = LinearLayout.LayoutParams(dp2px(100), dp2px(100))
+        layoutParams.setMargins(dp2px(10), dp2px(10), dp2px(10), dp2px(10))
+        flowLayout.addView(imageView, layoutParams)
+    }
+
+    fun dp2px(dp: Int): Int {
+        val metrics = resources.displayMetrics
+        return TypedValue.applyDimension(1, dp.toFloat(), metrics).toInt()
+    }
+
+
+    fun getNetworkService(): NetworkService {
+        val okHttpClient = OkHttpClient.Builder().build()
+        val retrofit = Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl("https://gitee.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        return retrofit.create(NetworkService::class.java)
     }
 }
